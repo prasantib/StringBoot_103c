a2470fdccbb5da2b2c5f67e5af9007ec359cc8b3
prasantib <prasanti.babu@gmail.com>
2019-10-07 14:53:19 -0400

    string boot 103c exercise

---
+++ src/main/java/com/example/demo/StringBoot103cApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringBoot103cApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringBoot103cApplication.class, args);
+    }
+
+}
